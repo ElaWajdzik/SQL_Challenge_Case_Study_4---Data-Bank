80c1915b03ac110e2bd1ebc15449bc6e6170750a
ElaWajdzik <ela.wajdzik@gmail.com>
2024-10-26 18:35:26 +0200

    Update Part C -  Data Allocation Challenge.sql

--- SQL code/Part C -  Data Allocation Challenge.sql
+++ SQL code/Part C -  Data Allocation Challenge.sql
@@ -4,7 +4,7 @@
 
 
 --Author: Ela Wajdzik
---Date: 23.10.2024
+--Date: 23.10.2024 (update 26.10.2024)
 --Tool used: Microsoft SQL Server
 
 
@@ -52,7 +52,7 @@ FROM customer_month_balances;
 WITH customer_balances AS (
 	SELECT 
 		*,
-		DATETRUNC(month, txn_date) AS month_date,
+		DATETRUNC(month, txn_date) AS txn_month,
 		SUM(CASE txn_type WHEN 'deposit' THEN 1 * txn_amount ELSE -1 * txn_amount END)
 		OVER(PARTITION BY customer_id ORDER BY txn_date, id_transaction) AS running_balance -- id_transaction aby nadać kolejność transakcji wykoananych tego samego dnia
 	FROM customer_transactions
@@ -79,32 +79,57 @@ WITH customer_month_balances AS (
 				ELSE txn_amount * (-1)
 			END) AS month_balance
 	FROM customer_transactions
-	-- WHERE customer_id IN (1, 2, 3, 4, 429, 109) -- a sample of customers
+	--WHERE customer_id IN (1, 2, 3) --, 4, 429, 109) -- a sample of customers
 	GROUP BY customer_id, DATETRUNC(month, txn_date)),
 
-customer_previous_month_balance AS (
+customer_closing_balances AS (
 	SELECT 
-		*,
-		LAG(month_balance, 1, 0) OVER (PARTITION BY customer_id ORDER BY txn_month) AS last_month_allocation
+		customer_id,
+		txn_month,
+		SUM(month_balance) OVER (PARTITION BY customer_id ORDER BY txn_month ASC) AS closing_balance  -- closing balance the value of total balance at the end of each month
+		--LAG(month_balance, 1) OVER (PARTITION BY customer_id ORDER BY txn_month) -- closing balance from previos month where customers were had activiti
 	FROM customer_month_balances)
 
 SELECT 
 	txn_month,
-	SUM(last_month_allocation) AS total_allocation,
-	SUM(CASE 
-			WHEN last_month_allocation < 0 THEN 0
-			ELSE last_month_allocation
-		END) AS total_positive_allocation
-FROM customer_previous_month_balance
+	SUM(closing_balance) AS total_data_allocated_per_month,
+	SUM(CASE WHEN closing_balance < 0 THEN 0 ELSE closing_balance END) AS total_positive_data_allocated_per_month
+FROM customer_closing_balances
 GROUP BY txn_month
 ORDER BY txn_month;
 
-
-
-
-
 -- Option 2: data is allocated on the average amount of money kept in the account in the previous 30 days
 
+WITH customer_balances AS (
+	SELECT 
+		*,
+		DATETRUNC(month, txn_date) AS txn_month,
+		SUM(CASE txn_type WHEN 'deposit' THEN 1 * txn_amount ELSE -1 * txn_amount END)
+		OVER(PARTITION BY customer_id ORDER BY txn_date, id_transaction) AS running_balance -- id_transaction aby nadać kolejność transakcji wykoananych tego samego dnia
+	FROM customer_transactions
+	--WHERE customer_id IN (1, 2, 3)
+	), -- a sample of customers
+
+customer_all_avg_balances AS (
+	SELECT 
+		customer_id,
+		txn_month,
+		AVG(running_balance) OVER (PARTITION BY customer_id, txn_month) AS avg_balance
+	FROM customer_balances),
+
+customer_avg_balances AS (
+	SELECT 
+		customer_id,
+		txn_month,
+		avg_balance
+	FROM customer_all_avg_balances
+	GROUP BY customer_id, txn_month, avg_balance)
+
+SELECT 
+	txn_month,
+	SUM(avg_balance) AS total_avg_data_allocated_per_month
+FROM customer_avg_balances 
+GROUP BY txn_month;
 
 
 -- Option 3: data is updated real-time
@@ -122,8 +147,8 @@ WITH customers_running_balance AS (
 
 SELECT 
 	txn_month,
-	SUM(running_balance) AS xx,
-	SUM(CASE WHEN running_balance < 0 THEN 0 ELSE running_balance END) AS xc
+	SUM(running_balance) AS total_data_allocated_per_month,
+	SUM(CASE WHEN running_balance < 0 THEN 0 ELSE running_balance END) AS total_positive_data_allocated_per_month
 FROM customers_running_balance
 GROUP BY txn_month
 ORDER BY txn_month;
